a45498ca25e57d58885950c78d585d8bcde5cea4
Swaraj Dash <swarajdash40@gmail.com>
2022-01-16 01:45:49 +0530

    mini challenge

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
-/**
-Challenge: 
+/*
+Mini Challenge:
+Move the `header` element from Page into 
+its own component called "Header"
+*/
 
-Part 2: 
-- Add a `header` element with a nested `nav` element. Inside the `nav`,
-  include a `img` element with the image of the React logo inside
-  (src="./react-logo.png") and make sure to set the width to something
-  more manageable so it doesn't take up the whole screen
-- Add an `h1` with some text describing the page. (E.g. "Reasons
-  I'm excited to learn React"). Place it above the ordered list.
-- Add a `footer` after the list that says: 
-    "© 20xx <last name here> development. All rights reserved."
-
- */
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png" width="70px" />
+            </nav>
+            <h1>Reaons I'm excited to learn React</h1>
+            <ol>
+                <li>Because I want to develop front-end apps</li>
+                <li>Without spending too much time</li>
+                <li>React has a huge community</li>
+                <li>Make good use of react libraries to develop complex features</li>
+            </ol>
+        </header>
+    )
+}
 
 function Page() {
-    return(
+    return (
         <div>
-            <header>
-                <nav>
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png" width="70px" />
-                </nav>
-                <h1>Reaons I'm excited to learn React</h1>
-                <ol>
-                    <li>Because I want to develop front-end apps</li>
-                    <li>Without spending too much time</li>
-                    <li>React has a huge community</li>
-                    <li>Make good use of react libraries to develop complex features</li>
-                </ol>
-            </header>
+            <Header />
             <footer><small>© 2022 SPD development. All rights reserved.</small></footer>
         </div>
     )
