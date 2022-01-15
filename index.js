7f7d9a4fde4198a22980c31e4ce7facd44e632d1
Swaraj Dash <swarajdash40@gmail.com>
2022-01-15 13:24:49 +0530

    role of ReactDOM

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
-// const page = (
-//     <div>
-//         <h1 className="header">This is JSX</h1>
-//         <p>This is a paragraph</p>
-//     </div>
-// )
+/**
+Challenge: find out what happens if we try to append JSX
+to our div#root using .append() instead of ReactDOM
 
-/* 
-Challenge: 
+1. Create a sample page in JSX (≥ 4 elements) and save them in a variable
+2. Select the div with the ID of "root" and use `.append()` to append
+   your JSX
+3. See if you can guess what will show up in the browser before running
+   the code
+4. See if you can explain what actually shows up in the browser
+ */
 
-Create a navbar in JSX:
-    - Use the semantic `nav` element as the parent wrapper
-    - Have an h1 element with the brand name of your "website"
-    - Insert an unordered list for the other nav elements
-        - Inside the `ul`, have three `li`s for "Pricing",
-        "About", and "Contact"
-    - Don't worry about styling yet - it'll just be plain-looking HTML for now
-*/
-
-const navbar = (
+const element = (
     <nav>
-    <h1>SPD6</h1>
-    <ul>
-        <li>Pricing</li>
-        <li>About</li>
-        <li>Contact</li>
-    </ul>
+        <h1>Swaraj Dash</h1>
+        <ol>
+            <li>Developer</li>
+            <li>Engineer</li>
+            <li>Football fan</li>
+        </ol>
     </nav>
 )
 
-ReactDOM.render(
-    navbar,
-    document.getElementById("root")
-)
+document.getElementById("root").append(element)
+
+/*
+You will see [object Object] in the browser
+The purpose of this exercise is to remind that JSX returns plain JS objects
+It is only when we render it using ReactDOM.render() are the objects rendered as real DOM objects
+*/
