86763c046ed6e763828996b270574848b1b0511c
Swaraj Dash <swarajdash40@gmail.com>
2022-01-15 12:59:18 +0530

    navbar challenge

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-const h1 = document.createElement("h1")
-h1.textContent = "Hello world"
-h1.className = "header"
-console.log(h1)
+// const page = (
+//     <div>
+//         <h1 className="header">This is JSX</h1>
+//         <p>This is a paragraph</p>
+//     </div>
+// )
 
-// <h1 class="header">
+/* 
+Challenge: 
 
-const element = <h1 className="header">This is JSX</h1>
-console.log(element)
+Create a navbar in JSX:
+    - Use the semantic `nav` element as the parent wrapper
+    - Have an h1 element with the brand name of your "website"
+    - Insert an unordered list for the other nav elements
+        - Inside the `ul`, have three `li`s for "Pricing",
+        "About", and "Contact"
+    - Don't worry about styling yet - it'll just be plain-looking HTML for now
+*/
 
-/*
-{
-    type: "h1", 
-    key: null, 
-    ref: null, 
-    props: {
-        className: "header", 
-        children: "This is JSX"
-    }, 
-    _owner: null, 
-    _store: {}
-}
- */
+const navbar = (
+    <nav>
+    <h1>SPD6</h1>
+    <ul>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+    </ul>
+    </nav>
+)
 
-// JSX
-ReactDOM.render(element, document.getElementById("root"))
+ReactDOM.render(
+    navbar,
+    document.getElementById("root")
+)
