069faec27a1784e8cf1068648b3ee2eff9f8df37
Swaraj Dash <swarajdash40@gmail.com>
2022-01-15 12:08:08 +0530

    first react practice

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"))
+ReactDOM.render(
+    <ul>
+        <li>One</li>
+        <li>Two</li>
+    </ul>
+    , document.getElementById("root"))
