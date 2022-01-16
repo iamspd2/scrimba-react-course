b9e8fc78a225a1102118c42a25f14c4768cff8c5
Swaraj Dash <swarajdash40@gmail.com>
2022-01-16 20:12:40 +0530

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 
-function App() {
-    return(
+export default function App() {
+    return (
         <div className="container">
             <Navbar />
             <Main />
         </div>
     )
 }
-
-export default App
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
-function Navbar() {
-    return(
-        <h1>Navbar goes here</h1>
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png" className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
     )
 }
-
-export default Navbar
